1772ffe4be3830662dbc4bbaad46877c9b5efc3a
stradlingm <matthew.stradling@dcmail.ca>
2026-04-21 21:07:32 -0400

    Update vite.config.js
    potential fix for base in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()], base: '/css-assignment-4-capstone/css/' })
+export default defineConfig({ plugins: [react()], base: '/css-assignment-4-capstone/' })
